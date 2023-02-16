74f5df68f9bb34715383658f948140ef6a0fb7fc
Gilliady <gilliadyazevedo@gmail.com>
2023-02-16 01:09:48 -0300

    finalizado todos os requisitos + bonus

--- script.js
+++ script.js
@@ -61,24 +61,37 @@ const getSelectedCheckBoxes = (checkBoxList) => {
   }
   return checkedList;
 };
+
+const createSubjectsMensage = (subjectsList) => {
+  let mensage = `${subjectsList[0]}, `;
+  for (let index = 1; index < subjectsList.length; index += 1) {
+    mensage += subjectsList[index];
+    if (index !== subjectsList.length - 1) {
+      mensage += ', ';
+    }
+  }
+  return mensage;
+};
+
 const name = document.querySelector('#input-name');
 const lastName = document.querySelector('#input-lastname');
 const emailInput = document.querySelector('#input-email');
 const houseLabel = document.querySelector('#house');
-
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
   const fullNameInput = `${name.value} ${lastName.value}`;
   const familyInput = document.getElementsByName('family');
   const familySelected = getSelectedRadioInput(familyInput);
   const materias = document.querySelectorAll('.techOptions');
-  const checkedList = getSelectedCheckBoxes(materias);
   const form = document.querySelector('#evaluation-form');
-  form.innerHTML = `<form id="form-data">
-  <p>Nome: ${fullNameInput}</p>
+  form.style.display = 'none';
+  const formData = form.parentElement.appendChild(document.createElement('form'));
+  formData.id = 'form-data';
+  formData.innerHTML = `<p>Nome: ${fullNameInput}</p>
   <p>Email: ${emailInput.value}</p>
   <p>Casa: ${houseLabel.value}</p>
   <p>Família: ${familySelected}</p>
-  <p>Matérias: ${checkedList}</p>
-  <p></p><p></p>`;
+  <p>Matérias: ${createSubjectsMensage(getSelectedCheckBoxes(materias))}</p>
+  <p>Avaliação: ${getSelectedRadioInput(document.querySelectorAll('.rate'))}</p>
+  <p>Observações: ${document.querySelector('textarea').value}</p></form>`;
 });
